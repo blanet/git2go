35a0dfbc0b471e2999b6e6c9682a229e14828ee1
默翁 <moweng.xx@antfin.com>
2020-06-18 12:03:12 +0800

    test
    fix

--- git_dynamic.go
+++ git_dynamic.go
@@ -4,7 +4,7 @@ package git
 
 /*
 #include <git2.h>
-#cgo pkg-config: libgit2
+#cgo LDFLAGS: -lgit2
 
 #if LIBGIT2_VER_MAJOR != 0 || LIBGIT2_VER_MINOR != 28
 # error "Invalid libgit2 version; this git2go supports libgit2 v0.28"
